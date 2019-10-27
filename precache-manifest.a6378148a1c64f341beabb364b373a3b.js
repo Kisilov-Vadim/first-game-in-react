41d059325eeabef94bef687d7807ec9c2f12554b
VadimK <djvadya16@gmail.com>
2019-10-27 14:28:23 +0200

    Updates

---
+++ precache-manifest.a6378148a1c64f341beabb364b373a3b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "43f4064bc71c08a90ecd0d06c0ffa46a",
+    "url": "/first-game-in-react/index.html"
+  },
+  {
+    "revision": "2c97123496c3c482eddc",
+    "url": "/first-game-in-react/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "8156aa43010659d9503b",
+    "url": "/first-game-in-react/static/js/2.e8d631e2.chunk.js"
+  },
+  {
+    "revision": "2c97123496c3c482eddc",
+    "url": "/first-game-in-react/static/js/main.a5dd57c1.chunk.js"
+  },
+  {
+    "revision": "54c53d26fb1ab2001485",
+    "url": "/first-game-in-react/static/js/runtime-main.f23fca82.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/first-game-in-react/static/media/logo.25bf045c.svg"
+  }
+]);
